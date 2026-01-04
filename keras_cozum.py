90641c9a6e0255056e91800384058d35a98f072c
Hakkı <128519908+Hakk1T@users.noreply.github.com>
2026-01-04 19:31:30 +0300

    Add files via upload

---
+++ keras_cozum.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import os
+
+# Dosya yolunu 
+base_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(base_dir, "final_model_garanti.keras")
+
+# 1. Modeli Yükleme
+print("Model dosyası okunuyor...")
+model = tf.keras.models.load_model(model_path)
+
+# 2. Ozet
+print("\nModelin Mimarisi:\n")
+model.summary()
+
+weights = model.layers[0].get_weights()[0]
+print(f"\nİlk katman ağırlık boyutu: {weights.shape}")
